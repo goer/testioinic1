e93f541cc81657e61347bfa7b1ea639d50cac3c0
benediktus15 <benedyctoes@gmail.com>
2018-02-14 12:23:40 +0700

    styling PageLogin

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { BeritaProvider } from '../../providers/berita/berita';
 
-/**
- * Generated class for the DetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-detail',
